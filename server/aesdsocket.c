9c30a5c946436c9258efda67ab5cc3ce8f12ffcc
Abdul Alsabbagh <atsabbagh@gmail.com>
2024-03-24 20:44:05 -0400

    Revert "Making the code more modular"
    This reverts commit 652bcc5b235201ebc5b5d524b8cb954477b4e6d5.

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -14,119 +14,156 @@
 #define PORT "9000"
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 
-volatile sig_atomic_t signal_received = 0; 
+volatile sig_atomic_t signal_received = 0; // Flag for signal handling
 
 void signal_handler(int sig) {
-  signal_received = 1;
-  syslog(LOG_INFO, "Caught signal, exiting");
+    signal_received = 1;
+    syslog(LOG_INFO, "Caught signal, exiting");
 }
 
-int setup_server_socket(const char *port) {
-  // Socket setup
-  int server_fd;
-  struct addrinfo *serverinfo;
-  struct addrinfo hints;
-  memset(&hints, 0, sizeof(hints));
+int main(int argc, char *argv[]) {
+    int daemonize = 0;
 
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-
-  int ret = getaddrinfo(NULL, port, &hints, &serverinfo);
-  if (ret != 0) {
-    syslog(LOG_ERR, "getaddrinfo error: %s", gai_strerror(ret)); // Use gai_strerror for more descriptive errors
-    return -1;
-  }
-
-  server_fd = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol);
-  if (server_fd < 0) {
-    perror("socket failed");
-    freeaddrinfo(serverinfo); 
-    return -1;
-  }
-
-  int opt = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-    perror("setsockopt failed");
-    freeaddrinfo(serverinfo);
-    close(server_fd);
-    return -1;
-  }
-
-  if (bind(server_fd, serverinfo->ai_addr, serverinfo->ai_addrlen) < 0) {
-    perror("bind failed");
-    freeaddrinfo(serverinfo);
-    close(server_fd);
-    return -1;
-  }
-
-  freeaddrinfo(serverinfo); // Free memory
-
-  if (listen(server_fd, 20) != 0) {
-    perror("listen failed");
-    close(server_fd);
-    return -1;
-  }
-
-  return server_fd;
-}
-
-// Handles client communication: receives, processes, sends
-int handle_client(int client_fd) {
-  int filedata = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0666);
-  if (filedata < 0) {
-    perror("open failed (receive)");
-    return -1;
-  }
-
-  if (receive_data(client_fd, filedata) < 0) { 
-    close(filedata);
-    return -1; 
-  }
-
-  close(filedata);
-
-  filedata = open(DATA_FILE, O_RDONLY, 0666);
-  if (filedata < 0) {
-    perror("open failed (send)");
-    return -1;
-  }
-
-  if (send_data(client_fd, filedata) < 0) {
-    close(filedata);
-    return -1;
-  }
-
-  close(filedata);
-  return 0;
-}
-
-int receive_data(int client_fd, int filedata) {
-  char buffer[1000];
-  int bytes_read;
-
-  while ((bytes_read = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
-    write(filedata, buffer, bytes_read);
-    if (strchr(buffer, '\n') != NULL) {
-      break;
+    // Process command line argument
+    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+        daemonize = 1;
     }
-  }
 
-  return (bytes_read < 0) ? -1 : 0; 
+    // Daemonize if required 
+    if (daemonize) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork failed");
+            exit(1);
+        } else if (pid > 0) {
+            exit(0); // Exit parent process
+        }
+
+    }
+
+    // Open syslog
+    openlog("aesdsocket", LOG_PID, LOG_USER);
+
+    //struct to accept connection
+    struct sockaddr peer;
+    socklen_t peersize = sizeof(peer);
+    memset(&peer, 0, peersize);
+
+    // Socket setup
+    int server_fd, client_fd;
+    struct addrinfo *serverinfo;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_protocol = 0;
+    hints.ai_addr = NULL;
+
+    int ret = getaddrinfo(NULL, PORT, &hints, &serverinfo);
+    if (ret != 0) {
+        //syslog(LOG_ERR, "couldn't get addressinfo", ret);
+        exit(1);
+    }
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket failed");
+        exit(1);
+    }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt failed");
+        exit(1);
+    }
+
+    if (bind(server_fd, serverinfo->ai_addr, serverinfo->ai_addrlen) < 0) {
+        perror("bind failed");
+        exit(1);
+    }
+
+    if (listen(server_fd, 20) != 0) {
+        perror("listen failed");
+        exit(1);
+    }
+
+    // Signal handling setup
+    struct sigaction signalaction;
+    memset(&signalaction, 0, sizeof(struct sigaction));
+    signalaction.sa_handler = signal_handler; 
+    int retvalsig = sigaction(SIGINT, &signalaction, NULL);
+    if (retvalsig != 0) 
+    {
+        syslog(LOG_ERR, "SIGINT Signal error %d", retvalsig);
+        exit(1);
+    }
+    retvalsig = sigaction(SIGTERM, &signalaction, NULL);
+    if (retvalsig != 0) 
+    {
+        syslog(LOG_ERR, "SIGTERM Signal error %d", retvalsig);
+        exit(1);
+    }
+
+    syslog(LOG_INFO, "Signal Handling set up complete.");
+    printf("Signal handling set up.\n");
+
+    while (signal_received == 0) 
+    {
+        client_fd = accept(server_fd, &peer, &peersize);
+        if (client_fd < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        //syslog(LOG_INFO, "Accepted connection from %u", peer.sa_data);
+
+        // Handle data reception and transmission
+        int filedata = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0666);
+        if (filedata < 0) {
+            perror("open failed");
+            close(client_fd);
+            continue; 
+        }
+        //receive data
+        char buffer[1000];
+        int bytes_read;
+        while ((bytes_read = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) 
+        { 
+            write(filedata, buffer, bytes_read);
+            if (strchr(buffer, '\n') != NULL)
+            {
+                break;
+            }
+        } 
+        close(filedata);
+        memset(buffer, 0, sizeof(buffer));
+        //send data
+        filedata = open(DATA_FILE, O_RDONLY, 0666);
+        if (filedata < 0) 
+        {
+            perror("open failed");
+            close(client_fd);
+            continue; 
+        }
+        while ((bytes_read = read(filedata, buffer, sizeof(buffer))) > 0)
+        {
+            send(client_fd, buffer, bytes_read, 0);
+        }
+        close(filedata);
+        shutdown(client_fd, SHUT_RDWR);
+        close(client_fd);
+        memset(buffer, 0, sizeof(buffer));
+        
+        //syslog(LOG_INFO, "Closed connection from %u", peer.sa_data);
+    } 
+
+    // Cleanup 
+    close(server_fd);
+    remove(DATA_FILE); 
+    shutdown(server_fd, SHUT_RDWR);
+    close(server_fd);
+    closelog();
+    return 0;
 }
-
-int send_data(int client_fd, int filedata) {
-  char buffer[1000];
-  int bytes_read;
-
-  while ((bytes_read = read(filedata, buffer, sizeof(buffer))) > 0) {
-    send(client_fd, buffer, bytes_read, 0);
-  }
-
-  return (bytes_read < 0) ? -1 : 0;
-}
-
-void graceful_shutdown(int client_fd) {
-  shutdown(client_fd, SHUT_RDWR);
-  close(client_fd);
-} 
