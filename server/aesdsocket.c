8c0612780d98d958d2bb09907257b63fc8ac7752
Abdul Alsabbagh <atsabbagh@gmail.com>
2024-04-17 22:39:07 -0400

    Attempting multithread again

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,169 +1,242 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <syslog.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <signal.h>
-#include <syslog.h>
-#include <fcntl.h>
 #include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <arpa/inet.h>
 
 #define PORT "9000"
+#define CONNECTIONS 20
+#define TIMESTAMP_INT 10
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 
-volatile sig_atomic_t signal_received = 0; // Flag for signal handling
+pthread_mutex_t file_mutex;
+int signal_received = 0;
 
-void signal_handler(int sig) {
-    signal_received = 1;
-    syslog(LOG_INFO, "Caught signal, exiting");
+int sockfd;  // Global variable to hold the socket file descriptor
+
+// Signal handler function
+void handle_signal(int sig) {
+    if (sig == SIGINT || sig == SIGTERM) {
+        //cleanup and close the socket
+        signal_received = 1;
+        syslog(LOG_INFO, "Caught signal, exiting");
+        close(sockfd);
+        remove(DATA_FILE);
+        closelog();
+        exit(EXIT_SUCCESS);
+    }
 }
 
-int main(int argc, char *argv[]) {
-    int daemonize = 0;
+// Daemonize the process
+void daemonize() {
+    pid_t pid, sid;
 
-    // Process command line argument
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-        daemonize = 1;
+    // Fork the parent process
+    pid = fork();
+
+    // If fork fails, exit with an error message
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Daemonize if required 
-    if (daemonize) {
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork failed");
-            exit(1);
-        } else if (pid > 0) {
-            exit(0); // Exit parent process
-        }
-
+    // If fork succeeds, terminate the parent process
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
     }
 
-    // Open syslog
-    openlog("aesdsocket", LOG_PID, LOG_USER);
-
-    //struct to accept connection
-    struct sockaddr peer;
-    socklen_t peersize = sizeof(peer);
-    memset(&peer, 0, peersize);
-
-    // Socket setup
-    int server_fd, client_fd;
-    struct addrinfo *serverinfo;
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_protocol = 0;
-    hints.ai_addr = NULL;
-
-    int ret = getaddrinfo(NULL, PORT, &hints, &serverinfo);
-    if (ret != 0) {
-        //syslog(LOG_ERR, "couldn't get addressinfo", ret);
-        exit(1);
+    // Create a new session and become the leader of the session
+    sid = setsid();
+    if (sid < 0) {
+        perror("setsid failed");
+        exit(EXIT_FAILURE);
     }
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket failed");
-        exit(1);
+    // Change the working directory to root
+    if (chdir("/") < 0) {
+        perror("chdir failed");
+        exit(EXIT_FAILURE);
     }
 
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt failed");
-        exit(1);
-    }
+    // Close standard file descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
 
-    if (bind(server_fd, serverinfo->ai_addr, serverinfo->ai_addrlen) < 0) {
-        perror("bind failed");
-        exit(1);
-    }
+void *timestamp_writer(void *arg) {
+    while (1) {
+        pthread_mutex_lock(&file_mutex);
 
-    if (listen(server_fd, 20) != 0) {
-        perror("listen failed");
-        exit(1);
-    }
-
-    // Signal handling setup
-    struct sigaction signalaction;
-    memset(&signalaction, 0, sizeof(struct sigaction));
-    signalaction.sa_handler = signal_handler; 
-    int retvalsig = sigaction(SIGINT, &signalaction, NULL);
-    if (retvalsig != 0) 
-    {
-        syslog(LOG_ERR, "SIGINT Signal error %d", retvalsig);
-        exit(1);
-    }
-    retvalsig = sigaction(SIGTERM, &signalaction, NULL);
-    if (retvalsig != 0) 
-    {
-        syslog(LOG_ERR, "SIGTERM Signal error %d", retvalsig);
-        exit(1);
-    }
-
-    syslog(LOG_INFO, "Signal Handling set up complete.");
-    printf("Signal handling set up.\n");
-
-    while (signal_received == 0) 
-    {
-        client_fd = accept(server_fd, &peer, &peersize);
-        if (client_fd < 0) {
-            perror("accept failed");
+        FILE *file = fopen(DATA_FILE, "a");
+        if (file == NULL) {
+            perror("File opening failed");
+            pthread_mutex_unlock(&file_mutex);
+            sleep(TIMESTAMP_INT);
             continue;
         }
 
-        //syslog(LOG_INFO, "Accepted connection from %u", peer.sa_data);
+        time_t now = time(NULL);
+        struct tm *time_data = localtime(&now);
+        char time_stamp[64];
+        strftime(time_stamp, sizeof(time_stamp), "timestamp:%Y-%m-%d %H:%M:%S\n", time_data);
 
-        // Handle data reception and transmission
-        int filedata = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0666);
-        if (filedata < 0) {
-            perror("open failed");
-            close(client_fd);
-            continue; 
-        }
-        //receive data
-        char buffer[1000];
-        int bytes_read;
-        while ((bytes_read = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) 
-        { 
-            write(filedata, buffer, bytes_read);
-            if (strchr(buffer, '\n') != NULL)
-            {
-                break;
+        fprintf(file, "%s", time_stamp);
+
+        fclose(file);
+        pthread_mutex_unlock(&file_mutex);
+
+        sleep(TIMESTAMP_INT);
+    }
+}
+
+void* connection_handler(void* socket_desc){
+    int new_fd = *(int *)socket_desc;
+
+    //lock mutex
+    pthread_mutex_lock(&file_mutex);
+
+    char buffer[1024];
+    ssize_t bytes_received;
+    int data_file_fd = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0666);
+    if (data_file_fd == -1) {
+        syslog(LOG_ERR, "open failed for %s", DATA_FILE);
+        close(new_fd);
+        pthread_exit(NULL);
+    } else {
+        while ((bytes_received = recv(new_fd, buffer, sizeof(buffer), 0)) > 0) {
+            write(data_file_fd, buffer, bytes_received);
+            // Check for newline character to consider the packet complete
+            if (memchr(buffer, '\n', bytes_received) != NULL) {
+                // Send the content of the file back to the client
+                int file_content_fd = open(DATA_FILE, O_RDONLY);
+                if (file_content_fd != -1) {
+                    while ((bytes_received = read(file_content_fd, buffer, sizeof(buffer))) > 0) {
+                        send(new_fd, buffer, bytes_received, 0);
+                    }
+                    close(file_content_fd);
+                }
+                lseek(data_file_fd, 0, SEEK_SET);  // Reset file offset for the next iteration
             }
-        } 
-        close(filedata);
-        memset(buffer, 0, sizeof(buffer));
-        //send data
-        filedata = open(DATA_FILE, O_RDONLY, 0666);
-        if (filedata < 0) 
-        {
-            perror("open failed");
-            close(client_fd);
-            continue; 
         }
-        while ((bytes_read = read(filedata, buffer, sizeof(buffer))) > 0)
-        {
-            send(client_fd, buffer, bytes_read, 0);
+        close(data_file_fd);
+        //unlock mutex
+        pthread_mutex_unlock(&file_mutex);
         }
-        close(filedata);
-        shutdown(client_fd, SHUT_RDWR);
-        close(client_fd);
-        memset(buffer, 0, sizeof(buffer));
-        
-        //syslog(LOG_INFO, "Closed connection from %u", peer.sa_data);
-    } 
+    // Close the connection
+    close(new_fd);
+    pthread_exit(NULL);
+}
 
-    // Cleanup 
-    close(server_fd);
-    remove(DATA_FILE); 
-    shutdown(server_fd, SHUT_RDWR);
-    close(server_fd);
+int main(int argc, char *argv[])
+{
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    struct addrinfo hints, *res;
+    int new_fd;
+
+
+    // Initialize the mutex
+    if (pthread_mutex_init(&file_mutex, NULL) != 0) {
+        perror("Mutex initialization failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open syslog
+    openlog(NULL, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+    // Set up signal handling for SIGINT and SIGTERM
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
+
+    // Daemonize if needed
+    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+        daemonize();
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;  
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
+        syslog(LOG_ERR, "getaddrinfo failed");
+        closelog();
+        exit(EXIT_FAILURE);
+    }
+
+    if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
+        syslog(LOG_ERR, "socket failed");
+        freeaddrinfo(res);
+        closelog();
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+        syslog(LOG_ERR, "bind failed");
+        close(sockfd);
+        freeaddrinfo(res);
+        closelog();
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sockfd, CONNECTIONS) == -1) {
+        syslog(LOG_ERR, "listen failed");
+        close(sockfd);
+        freeaddrinfo(res);
+        closelog();
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t timestamp_thread;
+    if (pthread_create(&timestamp_thread, NULL, timestamp_writer, NULL) < 0) {
+        perror("Timestamp thread failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // loop to accept connections
+    do {
+        // now accept an incoming connection:
+        addr_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+        if (new_fd == -1) {
+            syslog(LOG_ERR, "accept failed");
+            continue;  // Continue to the next iteration of the loop
+        }
+
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, connection_handler, (void *)&new_fd) < 0) {
+            perror("Thread failed");
+            exit(EXIT_FAILURE);
+        }
+        pthread_join(thread, NULL);
+
+        // Log accepted connection
+        char addr_str[INET6_ADDRSTRLEN];
+        inet_ntop(their_addr.ss_family,
+                  &(((struct sockaddr_in*)&their_addr)->sin_addr),
+                  addr_str, sizeof addr_str);
+        syslog(LOG_INFO, "Accepted connection from %s", addr_str);
+
+        // Log closed connection
+        syslog(LOG_INFO, "Closed connection from %s", addr_str);
+
+    } while (signal_received == 0);
+    
+    // Cleanup and close the socket
+    close(sockfd);
+    freeaddrinfo(res);
     closelog();
+
     return 0;
 }
